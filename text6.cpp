2bc206e392f917a6e2962ba6dc15d3f57ecdccf6
shappin <110470710+shappin@users.noreply.github.com>
2022-08-06 23:16:28 +0800

    Add files via upload

---
+++ text6.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#define nono 4
+int main()
+{
+	int letter;
+	double m = 333.3321;
+	int c = 121;
+	printf("%c\n", 121);
+	printf("%f\n", m);
+	printf("%E\n", m);
+	printf("%e\n", m);
+	letter= c + 3;
+	printf("letter=%d\n", letter);
+	int total;
+	int num = 10;
+	total = nono * num;
+	printf("total=%d\n",total);
+
+
+
+}
