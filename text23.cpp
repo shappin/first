8d35e5b8a4f4dae1c50e604aa6dcf0a0d7b7f0fd
shappin <110470710+shappin@users.noreply.github.com>
2022-08-20 12:15:32 +0800

    Add files via upload

---
+++ text23.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	//float a;
+	//while (1)
+	//{
+		//scanf("%f", &a);
+		//a++;
+		//printf("%.3f\n", a);
+	//}
+	for (float a;;)
+	{
+		
+		scanf("%f", &a);
+		a++;
+		printf("%.3f\n", a);
+	}
+
+
+
+}
